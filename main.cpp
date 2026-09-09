5e2163f5899b447ad362e18aa06820bf37e79ea5
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-09 08:54:06 +0700

    Input the number of students

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World" << endl;
+bool checkInputofStudents (int n);
 
+int main(){
+    int n;
+    cout << "How many students? ";
+    cin >> n;
+
+    if (checkInputofStudents(n) == true){
+        cout << n << endl;
+    }
+
+    else if (checkInputofStudents(n) == false){
+        cout << "Invalid input: " << endl; 
+        cout << "Invalid number of students!" << endl;
+        cout << "Please enter a number from 2 to 20" << endl;
+    }
     return 0;
+}
+
+bool checkInputofStudents (int n){
+    if (n >= 2 && n <= 20){
+        return true;
+    }
+    else return false;
 }
