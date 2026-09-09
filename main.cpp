c632fe12ed837f55d348f60beee1455fdd9bbabc
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-09 09:26:28 +0700

    Display the array

--- main.cpp
+++ main.cpp
@@ -1,42 +1,47 @@
 #include <iostream>
 using namespace std;
 
-bool checkInputofStudents (int n);
+bool checkInputofStudents(int n);
 
-
-int main(){
+int main() {
     int n;
     float score[20];
+    
     cout << "How many students? ";
     cin >> n;
-
-    if (checkInputofStudents(n) == true){
-        cout << n << endl;
-    }
-
-    else if (checkInputofStudents(n) == false){
+    
+    while (checkInputofStudents(n) == false) {
         cout << "Invalid input: " << endl; 
         cout << "Invalid number of students!" << endl;
         cout << "Please enter a number from 2 to 20" << endl;
+        cout << "How many students? ";
+        cin >> n;
     }
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << "Enter score for student " << i + 1 << ": ";
         cin >> score[i];
 
-        while (score[i] < 0 || score[i] > 10){
+        while (score[i] < 0 || score[i] > 10) {
             cout << "Invalid input: " << endl;
             cout << "Invalid score! Please enter a value from 0 to 10: ";
             cin >> score[i];
         }
     }
+
+    cout << endl;
+
+    cout << "===== STUDENT SCORES =====" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Student " << i + 1 << ": " << score[i] << endl;
+    }
+
     return 0;
 }
 
-bool checkInputofStudents (int n){
-    if (n >= 2 && n <= 20){
+bool checkInputofStudents(int n) {
+    if (n >= 2 && n <= 20) {
         return true;
     }
-    else return false;
+    return false;
 }
-
