8bf00fefb127c0e9b3c0d799d1fef2088c8a2146
Truong Tri Hung <25ad11008@student.hcmute.edu.vn>
2026-09-09 09:17:18 +0700

    Input scores for each student

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 bool checkInputofStudents (int n);
 
+
 int main(){
     int n;
+    float score[20];
     cout << "How many students? ";
     cin >> n;
 
@@ -17,6 +19,17 @@ int main(){
         cout << "Invalid number of students!" << endl;
         cout << "Please enter a number from 2 to 20" << endl;
     }
+
+    for (int i = 0; i < n; i++){
+        cout << "Enter score for student " << i + 1 << ": ";
+        cin >> score[i];
+
+        while (score[i] < 0 || score[i] > 10){
+            cout << "Invalid input: " << endl;
+            cout << "Invalid score! Please enter a value from 0 to 10: ";
+            cin >> score[i];
+        }
+    }
     return 0;
 }
 
@@ -26,3 +39,4 @@ bool checkInputofStudents (int n){
     }
     else return false;
 }
+
